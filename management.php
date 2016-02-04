f732f6699ba1d732f68ca4677d23f6ecc3710de3
Anshupriya2694 <anshupriya.srivastava@gmail.com>
2016-02-04 16:30:15 +0530

    Basic prototype implemeneted

---
+++ management.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<title>Management</title>
+<meta charset="iso-8859-1">
+<link rel="stylesheet" href="mang.css" type="text/css">
+<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+</head>
+<body>
+<div class="wrapper row1">
+  <header id="header" class="clear">
+    <div id="hgroup">
+      <h1><a href="#">Management Colleges</a></h1>
+      <h2>(India)</h2>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Careers</a></li>
+        <li class="last"><a href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+<div id="why">
+  <p>Why management?</p>
+  <pre>
+    High Salary Potential.
+
+    Be Your Own Boss.
+
+    Better Career Opportunities.
+
+    Ideal for Networking
+
+    Flexibility
+
+    Credibility
+    
+    Worldwide recognition of its credentials
+
+    Enhances Knowledge
+
+    Enhances Communication Skills
+
+    Personal Development
+   </pre>
+ </div>
+ 
+<form action="result.php" method="POST">
+      <div id = "state">
+      <p>Pick your College</p>
+      <h6>Select state</h6>
+      <select name = "name_state" id= "name_state">
+      <option value="none">-</option>
+  <option value="Andhra Pradesh">Andhra Pradesh</option>
+  <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+  <option value="Assam">Assam</option>
+  <option value="Bihar">Bihar</option>
+  <option value="Chhatisgarh">Chhatisgarh</option>
+  <option value="Goa">Goa</option>
+  <option value="Gujrat">Gujrat</option>
+  <option value="Haryana">Haryana</option>
+  <option value="Himachal Pradesh">Himachal Pradesh</option>
+  <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+  <option value="Jharkhand">Jharkhand</option>
+  <option value="Karntaka">Karnataka</option>
+  <option value="Kerela">Kerela</option>
+  <option value="Madhya Pradesh">Madhya Pradesh</option>
+  <option value="Maharashtra">Maharashtra</option>
+  <option value="Manipur">Manipur</option>
+  <option value="Meghalaya">Meghalaya</option>
+  <option value="Mizoram">Mizoram</option>
+  <option value="Nagaland">Nagaland</option>
+  <option value="Orissa">Orissa</option>
+  <option value="Punjab">Punjab</option>
+  <option value="Rajasthan">Rajasthan</option>
+  <option value="Sikkim">Sikkim</option>
+  <option value="Tamil Nadu">Tamil Nadu</option>
+  <option value="Telangana">Telangana</option>
+  <option value="Tripura">Tripura</option>
+  <option value="Uttar Pradesh">Uttar Pradesh</option>
+  <option value="Uttarakhand">Uttarakhand</option>
+  <option value="West Bengal">West Bengal</option>
+</select>
+<div id="marks">
+<p>Select marks</p>
+<select name="name_marks" id="name_marks">
+  <option value="none">-</option>
+  <option value="10">10-20</option>
+  <option value="20">21-30</option>
+  <option value="30">31-40</option>
+  <option value="40">41-50</option>
+  <option value="50">51-60</option>
+  <option value="60">61-70</option>
+  <option value="70">71-80</option>
+  <option value="80">80-91</option>
+  <option value="90">91-100</option>
+  </select>
+
+<div id="subject">
+<p>Select field</p>
+<select name="name_subject" id="name_subject"> 
+  <option value="none">-</option>
+  <option value="Finance">Finance</option>
+  <option value="Marketing">Marketing</option>
+  <option value="Sales">Sales</option>
+  <option value="HR">Human Resources</option>
+  <option value="IB">International Business</option>
+</select>
+<div id="button">
+<input type = "submit" class = "button" name="GO" value="GO"  />
+</div>
+</div>
+</div>
+</div>
+</form>
+    </article>
+    </section>
+    </div>
+    </div>
+  </header>
+</div>
---
+++ result.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+	$server = 'localhost';
+	$user = 'root';
+	$dbname = 'dbmsproject';
+	
+	$con = mysqli_connect($server, $user, "", $dbname);
+	if(mysqli_connect_errno()){
+	
+		echo "Failed to load database, ". mysqli_connect_error();
+	}
+	
+	$state = $_POST['name_state'];
+	$marks = $_POST['name_marks'];
+	$field = $_POST['name_subject'];
+		
+	$query = "SELECT name FROM college where cut_off >= ".$marks." AND category_id = 2 AND location =".$state;
+	$sql = mysqli_query($con,$query);
+	
+	while($result = mysqli_fetch_assoc($sql))
+	{
+		echo "{$result['name']}". "<br>";
+	}
+	
+?>
